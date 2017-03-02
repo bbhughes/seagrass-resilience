83e287a95eae8551dd788d63f3d870605e4139f3
Sean Anderson <sean@seananderson.ca>
2017-03-02 14:48:34 -0800

    Fixed label in figure one

--- analysis/3-hypotheses.R
+++ analysis/3-hypotheses.R
@@ -13,7 +13,7 @@ d <- mutate(d, hypothesis = sub("(hyp[0-9]+)_([a-z]+)", "\\1", response),
   type = sub("(hyp[0-9]+)_([a-z]+)", "\\2", response))
 
 d <- mutate(d, type = factor(type, levels = c("grazers", "algae", "eelgrass"),
-  labels = c("Grazer\nconsumption", "Algal\nbiomass", "Seagrass\nbiomass")))
+  labels = c("Grazer\nbiomass", "Algal\nbiomass", "Seagrass\nbiomass")))
 d <- mutate(d, hypothesis = factor(hypothesis, 
   labels = c("Hypothesis A", "Hypothesis B", "Hypothesis C")))
 
@@ -28,5 +28,5 @@ g <- ggplot(d, aes(pH, value)) +
   theme(panel.spacing = unit(-0.1, "lines")) +
   theme(axis.title = element_text(size = rel(0.8)))
 
-ggsave("figs/fig1.pdf", width = 3.7, height = 2.5)
+ggsave("figs/fig1.pdf", width = 3.8, height = 2.6)
 
