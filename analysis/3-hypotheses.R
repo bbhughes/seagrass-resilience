ae7dc934bb8b24c07f4d488a2e6d48c8c5f84d82
Sean Anderson <sean@seananderson.ca>
2017-03-14 10:31:13 -0700

    Add letters to figure one

--- analysis/3-hypotheses.R
+++ analysis/3-hypotheses.R
@@ -13,20 +13,27 @@ d <- mutate(d, hypothesis = sub("(hyp[0-9]+)_([a-z]+)", "\\1", response),
   type = sub("(hyp[0-9]+)_([a-z]+)", "\\2", response))
 
 d <- mutate(d, type = factor(type, levels = c("grazers", "algae", "eelgrass"),
-  labels = c("Grazer\nbiomass", "Algal\nbiomass", "Seagrass\nbiomass")))
+  labels = c("Grazer\nefficiency", "Algal\nbiomass", "Seagrass\nbiomass")))
 d <- mutate(d, hypothesis = factor(hypothesis, 
-  labels = c("Hypothesis A", "Hypothesis B", "Hypothesis C")))
+  labels = c("Hypothesis 1", "Hypothesis 2", "Hypothesis 3")))
+
+let <- unique(select(d, hypothesis, type))
+let <- mutate(let, letter = letters[seq_len(n())], x = 7.8, y = 4.75)
 
 g <- ggplot(d, aes(pH, value)) +
   geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = FALSE, 
     colour = "grey50", lwd = 0.7) +
   facet_grid(type~hypothesis, switch = "y") +
   theme_sleek() +
-  xlab("pH") + ylab("") +
-  theme(axis.ticks.x=element_blank(), axis.ticks.y=element_blank(), 
+  xlab(expression(pH[T])) + ylab("") +
+  geom_text(data = let, aes(x = x, y = y, label = letter), color = "grey40") +
+  theme(axis.ticks.x=element_blank(), axis.ticks.y=element_blank(),
     axis.text.y = element_blank(), axis.text.x = element_blank()) +
   theme(panel.spacing = unit(-0.1, "lines")) +
-  theme(axis.title = element_text(size = rel(0.8)))
+  theme(axis.title = element_text(size = rel(0.8))) +
+  scale_x_reverse()
 
-ggsave("figs/fig-1.pdf", width = 3.8, height = 2.6)
+print(g)
+
+ggsave("figs/fig-1.pdf", width = 4, height = 2.8)
 
