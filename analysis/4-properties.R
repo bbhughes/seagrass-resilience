216861eeabee7d69a156da1b3eed20aab43b75c0
Sean Anderson <sean@seananderson.ca>
2017-03-16 16:32:17 -0700

    Tweak and then scrap properties plot

--- analysis/4-properties.R
+++ analysis/4-properties.R
@@ -34,7 +34,15 @@ cols <- c(brewer.pal(4, "Greys")[[3]], brewer.pal(4, "Blues")[[3]])
 d <- mutate(d, nutrients_text = 
     ifelse(Nutrients == 0, "No Nutrients", "Nutrients added"))
 
-filter(d, variable != "Spec_pHT") %>%
+# limits <- tribble(
+  # ~variable,                  ~log_transform,  ~family,     ~link,
+  # "Spec_pHT",          TRUE,            "gaussian",  "identity",
+  # "NO3",  FALSE,           "gaussian",  "identity",
+  # "PO4",   FALSE,           "gaussian",  "identity",
+  # "TA"
+
+# filter(d, variable != "Spec_pHT") %>%
+filter(d) %>%
   ggplot(aes(ph, mean, fill = as.factor(nutrients_text))) + 
   geom_ribbon(aes(ymin = mean - sd, ymax = mean + sd), alpha = 0.3) +
   geom_line(aes(color = as.factor(nutrients_text))) +
@@ -44,8 +52,8 @@ filter(d, variable != "Spec_pHT") %>%
     x = expression(Acidification~(mean~seawater~pH[T]))) +
   scale_color_manual(values = cols) + 
   scale_fill_manual(values = cols) +
-  theme(legend.position = c(0.68, 0.54)) +
+  theme(legend.position = c(0.68, 0.11)) +
   # guides(fill = guide_legend(override.aes = list(fill = NA))) +
   scale_x_reverse() 
 
-ggsave("figs/properties.pdf", height = 4.2, width = 5)
+ggsave("figs/properties.pdf", height = 5.3, width = 5)
