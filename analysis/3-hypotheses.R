5b2234b1ecf8583f2118f95f3ced96299085a8ff
Sean Anderson <sean@seananderson.ca>
2017-06-29 16:18:25 -0700

    Finish tweaking hypothesis figure

--- analysis/3-hypotheses.R
+++ analysis/3-hypotheses.R
@@ -2,7 +2,6 @@
 
 library(tidyverse)
 library(ggsidekick) # devtools::install_github("seananderson/ggsidekick")
-# install("~/src/ggsidekick/")
 
 d <- read_csv("data/seagrass_mesocosm_hypotheses.csv", na = c("NA", "na"))
 d <- na.omit(d) %>%
@@ -11,23 +10,12 @@ d <- na.omit(d) %>%
 x <- d$pH
 x_centered <- x - mean(x)
 quad <- x_centered ^ 2 * -10
-# quad2 <- x_centered * -2 + x_centered ^ 2 * -10 - 0.08
 quad2 <- (x_centered) * 2 + (x_centered) ^ 2 * -10 - 0.1
-# plot(x, quad, type = "l")
-# lines(x, quad2, lty = 2)
-# linear <- exp(((x - mean(x)) ^ 1) * 2)
-# plot(x, linear)
 linear <- x_centered * -2
 linear2 <- x_centered * -3.5 + 0.75
 
 scale01 <- function(x) {(x - min(x)) / (max(x) - min(x))}
 
-# x <- seq(min(d$pH), max(d$pH), length.out = 100)
-# y1 <- (x - 5) ^ 2 * (-5)
-# # y2 <- (x - 5) * -20 + (x - 5) ^ 2 * (-5) - 20
-# y2 <- y1
-# # plot(x, y1, type = "p")
-# # lines(x, y2)
 
 gap <- 0.05
 dat <- data.frame(pH = c(x, x), 
@@ -37,7 +25,8 @@ dat <- data.frame(pH = c(x, x),
   hyp2_algae = c(linear, linear2), 
   hyp1_eelgrass = c(quad, quad2), 
   hyp2_eelgrass = c(linear, linear2), 
-  nutrients = c(rep(FALSE, length(x_centered)), rep(TRUE, length(x_centered))))
+  nutrients = c(rep("No Nutrients", length(x_centered)), 
+    rep("Nutrients added", length(x_centered))))
 
 d <- dat %>% 
   gather(response, value, -pH, -nutrients)
@@ -56,29 +45,30 @@ d <- d %>% group_by(type, hypothesis) %>%
 
 let <- unique(dplyr::select(d, hypothesis, type))
 let <- as.data.frame(let)
-let <- dplyr::mutate(let, letter = LETTERS[seq_len(nrow(let))], x = rep(7.82, nrow(let)), 
-  y = rep(0.9, nrow(let)), nutrients = FALSE)
+let <- dplyr::mutate(let, letter = letters[seq_len(nrow(let))], x = rep(7.82, nrow(let)), 
+  y = rep(0.9, nrow(let)), nutrients = "No Nutrients")
 
 g <- ggplot(d, aes(pH, value, lty = nutrients)) +
-  # geom_rect(data = subset(let, !letter %in% c("F", "H", "I")), aes(x = x, y = y),
-    # fill = "grey95",
-    # xmin = -Inf,xmax = Inf, ymin = -Inf, ymax = Inf) +
   geom_smooth(method = "lm", formula = y ~ poly(x, 2), se = FALSE,
     colour = "grey50", lwd = 0.7) +
   facet_grid(type~hypothesis, switch = "y") +
   theme_sleek() +
   ylab("") +
-  scale_linetype_manual(values = c(3, 1)) +
+  scale_linetype_manual(values = c(2, 1)) +
   xlab(expression(Acidification~(seawater~pH[T]))) +
   geom_text(data = let, aes(x = x, y = y, label = letter), color = "grey40") +
   theme(axis.ticks.y=element_blank(),
     axis.text.y = element_blank()) +
   theme(panel.spacing = unit(-0.1, "lines")) +
   theme(axis.title = element_text(size = rel(0.8))) +
-  # scale_x_reverse(breaks = c(7.2, 7.7), labels = c(expression(Low~pH[T]), expression(High~pH[T]))) +
+  theme(legend.position = c(0.25, 0.12 )) +
   scale_x_reverse(breaks = c(7.2, 7.7)) +
-  labs(linetype = "Nutrients added")
+  labs(linetype = "") +
+  theme(legend.text = element_text(size = 7.7)) +
+  theme(legend.key.width = unit(1.35, "line")) +
+  theme(strip.background = element_blank(),
+    strip.text.y = element_blank())
 print(g)
 
-# ggsave("figs/fig-1.pdf", width = 4, height = 2.8)
+ggsave("figs/fig-1.pdf", width = 3.2, height = 3.3, plot = g)
 
